6825f3a71e296970cca0b840426ace7d9cd8d45a
Surya-Hitesh-Nalam <suryanalam1234@gmail.com>
2024-12-18 21:46:07 +0530

    admin ui

--- frontend/src/admin/AddMenu.tsx
+++ frontend/src/admin/AddMenu.tsx
@@ -1,15 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Loader2, Plus } from "lucide-react"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import EditMenu from "./EditMenu"
+import { MenuFormSchema, menuSchema } from "@/schema/menuSchema"
+
+const menu =[
+    {
+        name:"Biriyani",
+        description:"lorem blahbla habla ha",
+        price:80,
+        image:"https://content.jdmagicbox.com/comp/def_content/pure_veg_restaurants/default-pure-veg-restaurants-5.jpg?clr=#5c3b0a" ,
+    }
+]
 
 const AddMenu = () => {
+    const [input,setInput] = useState<MenuFormSchema>({
+        name:"",
+        description:"",
+        price:0,
+        image:undefined
+    })
     const [open,setOpen] = useState<boolean>(false)
+    const [editOpen,setEditOpen] = useState<boolean>(false)
     const [selectedMenu,setSelectedMenu] = useState<any>()
+    const [error,setError] = useState<Partial<MenuFormSchema>>({})
     const loading=false
+
+    const changeEventHandler = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        const {name,value,type}=e.target;
+        setInput({...input,[name]:type==='number'?Number(value):value})
+    }
+
+    const submitHandler = (e:FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+        const result = menuSchema.safeParse(input);
+        if(!result.success){
+            const fieldErrors = result.error.formErrors.fieldErrors;
+            setError(fieldErrors as Partial<MenuFormSchema>)
+            return;
+        }
+    }
   return (
     <div className="max-w-6xl mx-auto my-10">
         <div className="flex justify-between">
@@ -26,22 +61,22 @@ const AddMenu = () => {
                     </DialogTitle>
                     <DialogDescription>Create a menu that will make your restaurant standout</DialogDescription>
                 </DialogHeader>
-                <form action="">
+                <form action="" onSubmit={submitHandler} className="space-y-4">
                     <div>
                         <Label>Name</Label>
-                        <Input type="text" placeholder="enter menu name" name="name"/>
+                        <Input type="text" placeholder="enter menu name" value={input.name} onChange={changeEventHandler} name="name"/>
                     </div>
                     <div>
                         <Label>Description</Label>
-                        <Input type="text" placeholder="enter menu description" name="description"/>
+                        <Input type="text" placeholder="enter menu description" value={input.description} onChange={changeEventHandler} name="description"/>
                     </div>
                     <div>
                         <Label>Price in (Rupees)</Label>
-                        <Input type="text" placeholder="enter menu price" name="price"/>
+                        <Input type="number" placeholder="enter menu price" value={input.price} onChange={changeEventHandler} name="price"/>
                     </div>
                     <div>
                         <Label>Upload Menu Image</Label>
-                        <Input type="file" placeholder="enter menu name" name="image"/>
+                        <Input type="file" placeholder="enter menu name" onChange={(e)=>setInput({...input,image:e.target.files?.[0]||undefined})} name="image"/>
                     </div>
                     <DialogFooter>
                         {
@@ -58,21 +93,30 @@ const AddMenu = () => {
             </DialogContent>
         </Dialog>
         </div>
-        <div className="mt-6 space-y-4">
+        {
+            menu.map((menu:any,idx:number)=>(
+                <div className="mt-6 space-y-4">
             <div className="flex flex-col md:flex-row md:items-center md:space-x-4 md:p-4 p-2 shadow-md rounded-lg border">
-                <img src="https://content.jdmagicbox.com/comp/def_content/pure_veg_restaurants/default-pure-veg-restaurants-5.jpg?clr=#5c3b0a" 
+                <img src={menu.image}
                 alt="" 
                 className="md:h-24 md:w-24 h-16 w-full object-cover rounded-lg"
                 />
                 <div className="flex-1">
-                    <h1 className="text-lg font-semibold text-gray-800">Biriyani</h1>
-                    <p className="text-sm text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <h2 className="text-md font-semibold mt-2">Price:<span className="text-[#D19254]">80</span></h2>
+                    <h1 className="text-lg font-semibold text-gray-800">{menu.name}</h1>
+                    <p className="text-sm text-gray-600">{menu.description}</p>
+                    <h2 className="text-md font-semibold mt-2">Price:<span className="text-[#D19254]">{menu.price}</span></h2>
                 </div>
-                <Button size={'sm'} className="bg-orange hover:bg-hoverOrange mt-2">Edit</Button>
+                <Button onClick={()=>{
+                    setSelectedMenu(menu);
+                    setEditOpen(true);
+                }} size={'sm'} className="bg-orange hover:bg-hoverOrange mt-2">Edit</Button>
             </div>
         </div>
-        <EditMenu selectedMenu={selectedMenu}/>
+            ))
+        }
+        
+        
+        <EditMenu selectedMenu={selectedMenu} editOpen = {editOpen} setEditOpen={setEditOpen}/>
     </div>
   )
 }
--- frontend/src/admin/EditMenu.tsx
+++ frontend/src/admin/EditMenu.tsx
@@ -1,8 +1,78 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { MenuFormSchema } from "@/schema/menuSchema"
+import { Label } from "@radix-ui/react-menubar"
+import { Loader2 } from "lucide-react"
+import React, { Dispatch, FormEvent, SetStateAction, useEffect, useState } from "react"
 
 
-const EditMenu = () => {
+const EditMenu = ({selectedMenu,editOpen,setEditOpen}:{selectedMenu:MenuFormSchema,editOpen:boolean,setEditOpen:Dispatch<SetStateAction<boolean>>}) => {
+
+  const [input,setInput] = useState<MenuFormSchema>({
+          name:"",
+          description:"",
+          price:0,
+          image:undefined
+      })
+  
+  const loading=false
+  const changeEventHandler =(e:React.ChangeEvent<HTMLInputElement>)=>{
+    const {name,value,type}=e.target
+    setInput({...input,[name]:type==='number'?Number(value):value})
+  }
+  const submitHandler=(e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+  }
+
+  useEffect(()=>{
+    setInput({
+      name:selectedMenu?.name || "",
+      description:selectedMenu?.description || "",
+      price:selectedMenu?.price || 0,
+      image:undefined
+    })
+  },[selectedMenu])
+
   return (
-    <div>EditMenu</div>
+    <Dialog open={editOpen} onOpenChange={setEditOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>edit Menu</DialogTitle>
+          <DialogDescription>Update your name to keep your offering fresh and exciting!</DialogDescription>
+        </DialogHeader>
+        <form action="" onSubmit={submitHandler} className="space-y-4">
+                    <div>
+                        <Label>Name</Label>
+                        <Input type="text" placeholder="enter menu name" value={input.name} onChange={changeEventHandler} name="name"/>
+                    </div>
+                    <div>
+                        <Label>Description</Label>
+                        <Input type="text" placeholder="enter menu description" value={input.description} onChange={changeEventHandler} name="description"/>
+                    </div>
+                    <div>
+                        <Label>Price in (Rupees)</Label>
+                        <Input type="number" placeholder="enter menu price" value={input.price} onChange={changeEventHandler} name="price"/>
+                    </div>
+                    <div>
+                        <Label>Upload Menu Image</Label>
+                        <Input type="file" placeholder="enter menu name" onChange={(e)=>setInput({...input,image:e.target.files?.[0]||undefined})} name="image"/>
+                    </div>
+                    <DialogFooter>
+                        {
+                            loading?(
+                                <Button disabled className="bg-orange hover:bg-hoverOrange"><Loader2 className="mr-2 w-4 h-4 animate-spin"/>Please Wait</Button>
+
+                            ):(
+                                <Button className="bg-orange hover:bg-hoverOrange">Submit</Button>
+
+                            )
+                        }
+                    </DialogFooter>
+                </form>
+      </DialogContent>
+    </Dialog>
   )
 }
 
